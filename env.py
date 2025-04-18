1ee72449c5ff2853b18b3497d1a9ed006e13fba2
Mahdii04 <am.tahir@esi-sba.dz>
2025-04-18 10:12:23 +0100

    Env Modification

--- env.py
+++ env.py
@@ -6,13 +6,30 @@ class TextEnv:
     def __init__(self, rom_path):
         self.env = jericho.FrotzEnv(rom_path)
         self.state = None
-        self.vocab = self.env.get_dictionnary()
+        self.vocab = self.env.get_dictionary()
         self.vocab_mapping = {str(self.vocab[i]): i for i in range(len(self.vocab))}
         self.action_generator =  TemplateActionGenerator(self.env.bindings)
 
     def reset(self):
         self.state = self.env.reset()
-        return self.state
+        reward = 0
+        done = False
+
+        self.state.replace('\n', ' ')
+
+        inv_desc, _, _, _ = self.env.step("inventory")
+        inv_desc.replace('\n', ' ')
+
+        description, _, _, _ = self.env.step("look")
+        description.replace('\n', ' ')
+
+        return  {
+            'observation': self.state,
+            'description': description,
+            'inventory': inv_desc,
+            'reward': reward,
+            'done': done
+        }
         
     def step(self, action):
         
