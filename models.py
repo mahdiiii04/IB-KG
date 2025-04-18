d0a1fa6965f83b7042a95c9a56ffca74a0a51146
Mahdii04 <am.tahir@esi-sba.dz>
2025-04-18 10:57:52 +0100

    Modified RGCN

--- models.py
+++ models.py
@@ -28,8 +28,8 @@ class TextModel:
 class RGCN(nn.Module):
     def __init__(self, in_dim, hidden_dim, out_dim, num_rels):
         super(RGCN, self).__init__()
-        self.layer1 = RelGraphConv(in_dim, hidden_dim, num_rels, "sum")
-        self.layer1 = RelGraphConv(hidden_dim, out_dim, num_rels, "sum")
+        self.layer1 = RelGraphConv(in_dim, hidden_dim, num_rels, "basis")
+        self.layer1 = RelGraphConv(hidden_dim, out_dim, num_rels, "basis")
 
     def forward(self, graph, feat, etypes):
         x = self.layer1(graph, feat, etypes)
